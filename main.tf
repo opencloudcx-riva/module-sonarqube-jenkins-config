16159df54fce1176216b3817c4964539901d86ba
Adam Norris <anorris@rivasolutionsinc.com>
2022-02-28 22:33:59 -0500

    Added sonarqube cprovider configuration

--- main.tf
+++ main.tf
@@ -4,7 +4,16 @@ terraform {
       source  = "taiidani/jenkins"
       version = "~> 0.9.0"
     }
+    sonarqube = {
+      source  = "jdamata/sonarqube"
+      version = "0.0.11"
+    }
   }
+}
+
+resource "sonarqube_webhook" "webhook" {
+  name = "jenkins-webhook"
+  url  = "http://jenkins.jenkins.svc.cluster.local:8080/sonarqube-webhook/"
 }
 
 resource "jenkins_credential_secret_text" "sonarqube-api-key" {
